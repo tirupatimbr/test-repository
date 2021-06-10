61113231fa891f28bb70c1a3b0b02af2c35670c1
tirupatimbr <tirupatimbr@gmail.com>
2021-06-10 17:21:35 +0530

    mmmm

--- hub.py
+++ hub.py
@@ -2,14 +2,12 @@
 hungry=input('Are you hungry ? :')
 if hungry == 'yes':
 	print('eat samosa')
-	print('eat egg')
+	
 else:
 	thirsty = input('Are You thirsty? : ')
 	if thirsty == 'yes':
 		print('drink water')
 		print('pepsi')	
-		print('maaza')
-		print('cocacola')
-
+	
 	else:
 		print('do work')	
