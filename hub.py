9e2fd46ecd0fd7405276826eaebb214febe6a901
tirupatimbr <tirupatimbr@gmail.com>
2021-06-10 16:57:11 +0530

    delete chicken n pizza

--- hub.py
+++ hub.py
@@ -2,13 +2,12 @@
 hungry=input('Are you hungry ? :')
 if hungry == 'yes':
 	print('eat samosa')
-	print('eat chicken')
-	print('pizza')
 else:
 	thirsty = input('Are You thirsty? : ')
 	if thirsty == 'yes':
 		print('drink water')
 		print('pepsi')	
 		print('maaza')
+		print('cocacola')
 	else:
 		print('do work')	
